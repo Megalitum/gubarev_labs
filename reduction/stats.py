8a57bf0ee63505218acb2a6a433b43d9ec40458d
Vlad Fatenko <vladf@compatibl.com>
2017-06-30 15:23:52 +0300

    Eigs plot fix.

--- reduction/stats.py
+++ reduction/stats.py
@@ -26,10 +26,13 @@ def general_test(observable, reductor, tick_count, reduced_dim, methods=None):
     MSE: {mse}\n""".format(method=method,**fit_report_results)))
 
 
+
 def eigenvalues_fit_report(eig_ref, eig_est):
     figure()
+    xlabel('Re')
+    ylabel('Im')
     scatter(np.stack((eig_ref.real, eig_ref.real)),
-            np.stack((eig_ref.imag, -eig_ref.imag)), c='black', marker='+', label='input')
+            np.stack((eig_ref.imag, -eig_ref.imag)), c='black', marker='o', label='input')
     scatter(np.stack((eig_est.real, eig_est.real)),
             np.stack((eig_est.imag, -eig_est.imag)), c='black', marker='x', label='output')
     legend()
